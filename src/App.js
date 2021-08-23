f2606982c1bc9276d10e3940e5048c0c91ba14c6
Rafael Costa <rafaelsevla@gmail.com>
2021-08-23 17:25:46 -0300

    abcd

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import React from "react";
 const App = () => (
   <div>
     {console.log('node.', process.env.NODE_ENV)}
-    <h1>Dynamic System Host</h1>
+    <h1>Dynamic Systsssssssem Host</h1>
     <h2>App 2</h2>
     <LocalButton />
     <Card />
