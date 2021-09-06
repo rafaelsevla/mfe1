46c1ef88b7cf85ff41c561052058cd6f24c9840b
Rafael Costa <rafaelsevla@gmail.com>
2021-09-06 11:29:12 -0300

    remove some files add eslint etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import React from "react";
 import LocalButton from "./Widget";
 import Card from "./Card";
 import Circle from "./Circle";
-import React from "react";
+
+
 
 const App = () => (
   <div>
     {console.log('node.', process.env.NODE_ENV)}
     <h1>Dynamic Systsssssssem Host</h1>
     <h2>App 2</h2>
+    <button>
+      ushshu
+    </button>
     <LocalButton />
     <Card />
     <Circle />
