c28b33a16ee0682fb34e3a2aebab302023192dfd
Rafael Costa <rafaelsevla@gmail.com>
2021-10-19 09:42:23 -0300

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin =
   require("webpack").container.ModuleFederationPlugin;
 const path = require("path");
-const deps = require("./package.json").dependencies;
+
 module.exports = {
   entry: "./src/index",
   mode: "development",
@@ -23,7 +23,7 @@ module.exports = {
         loader: "bundle-loader",
         options: {
           lazy: true,
-        }
+        },
       },
       {
         test: /\.tsx?$/,
